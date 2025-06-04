864eedf12c57b530b18cc2befa0ce6d0672a6deb
Josip Bulić Lokalni Admin <josip.bulic@asee.io>
2025-06-04 16:25:22 +0200

    Add authentication handlers

--- handlers/auth_handlers.go
+++ handlers/auth_handlers.go
@@ -1,1 +1,41 @@
 package handlers
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"zavrsni/yo-yo-car/models"
+	"zavrsni/yo-yo-car/utils"
+)
+
+func Login(c *gin.Context) {
+	var user models.User
+
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid data"})
+		return
+	}
+
+	// Check if credentials are valid (replace this logic with real authentication)
+	if user.Email == "email" && user.Password == "password" {
+		token, err := utils.GenerateToken(user.ID)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error generating token"})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"token": token})
+	} else {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid credentials"})
+	}
+}
+
+func Register(c *gin.Context) {
+	var user models.User
+
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid data"})
+	}
+
+	user.ID =
+		c.JSON(http.StatusCreated, gin.H{"message": "User registered successfully"})
+}
