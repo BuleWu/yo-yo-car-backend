ceaa0f144e1927532ce73e9a4b20117015bdbe26
Josip Bulić Lokalni Admin <josip.bulic@asee.io>
2025-06-04 15:23:28 +0200

    Add user model

---
+++ models/user.go
@@ -0,0 +1,9 @@
+package models
+
+type User struct {
+	ID        uint   `json:"id" gorm:"primaryKey"`
+	FirstName string `json:"first_name" gorm:"column:first_name"`
+	LastName  string `json:"last_name" gorm:"column:last_name"`
+	Email     string `json:"email" gorm:"uniqueIndex"`
+	Password  string `json:"-" gorm:"column:password"`
+}
