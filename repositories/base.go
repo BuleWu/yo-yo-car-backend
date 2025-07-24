25c66d1dba5fb8e9b6f65c0a1c1086e84132f24d
Josip Bulić <josip.bulic@asee.io>
2025-07-24 14:30:56 +0200

    Remove unused imports

--- repositories/base.go
+++ repositories/base.go
@@ -1,9 +1,5 @@
 package repositories
 
-import (
-	_ "github.com/jinzhu/gorm/dialects/postgres"
-)
-
 type SearchQuery struct {
 	Column   string      `json:"column"`
 	Operator string      `json:"operator"`
