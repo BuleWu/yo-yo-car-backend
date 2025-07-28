b398a213110bfd032feb90874278ad2fdb740c1d
Josip Bulić <josip.bulic@asee.io>
2025-07-28 18:36:41 +0200

    Add reservation repository

---
+++ repositories/reservation.go
@@ -0,0 +1,66 @@
+package repositories
+
+import (
+	"zavrsni/yo-yo-car/database"
+	"zavrsni/yo-yo-car/models"
+)
+
+func NewReservation(db *database.Connection) *Reservation {
+	return &Reservation{
+		conn: db,
+	}
+}
+
+type ReservationRepository interface {
+	Persist(reservation *models.Reservation) (*models.Reservation, error)
+	GetById(id string) (*models.Reservation, error)
+	GetAll() ([]*models.Reservation, error)
+	Update(reservation *models.Reservation) (*models.Reservation, error)
+	Delete(reservation *models.Reservation) error
+}
+
+type Reservation struct {
+	conn *database.Connection
+}
+
+func (repo *Reservation) Persist(reservation *models.Reservation) (*models.Reservation, error) {
+	db := repo.conn.GetConnection()
+	if err := db.Create(reservation).Error; err != nil {
+		return nil, err
+	}
+	return reservation, nil
+}
+
+func (repo *Reservation) GetById(id string) (*models.Reservation, error) {
+	db := repo.conn.GetConnection()
+	var reservation models.Reservation
+	if err := db.First(&reservation, "id = ?", id).Error; err != nil {
+		return nil, err
+	}
+	return &reservation, nil
+}
+
+func (repo *Reservation) GetAll() ([]*models.Reservation, error) {
+	db := repo.conn.GetConnection()
+	var reservations []*models.Reservation
+	if err := db.Find(&reservations).Error; err != nil {
+		return nil, err
+	}
+	return reservations, nil
+}
+
+func (repo *Reservation) Update(reservation *models.Reservation) (*models.Reservation, error) {
+	db := repo.conn.GetConnection()
+	if err := db.Save(reservation).Error; err != nil {
+		return nil, err
+	}
+	return reservation, nil
+}
+
+func (repo *Reservation) Delete(reservation *models.Reservation) error {
+	db := repo.conn.GetConnection()
+	if err := db.Delete(reservation).Error; err != nil {
+		return err
+	}
+	return nil
+}
